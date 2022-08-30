0ad0850c6113a521d18a989f76e95013eaee944c
GuojianZou <32932644+zouguojian@users.noreply.github.com>
2022-08-30 16:02:08 +0800

    first

--- model/hyparameter.py
+++ model/hyparameter.py
@@ -17,7 +17,7 @@ class parameter(object):
         self.parser.add_argument('--dropout', type=float, default=0.0, help='drop out')
         self.parser.add_argument('--site_num', type=int, default=108, help='total number of road')
         self.parser.add_argument('--k', type=int, default=64, help='latent vector dimension')
-        self.parser.add_argument('--field_cnt', type=int, default=17, help='the number of filed')
+        self.parser.add_argument('--field_cnt', type=int, default=17, help='the number of filed for trajectory features')
         self.parser.add_argument('--feature_tra', type=int, default=30542, help='number of the trajectory feature elements')
         self.parser.add_argument('--trajectory_length', type=int, default=5, help='length of trajectory')
         self.parser.add_argument('--num_heads', type=int, default=4, help='total number of head attentions')
